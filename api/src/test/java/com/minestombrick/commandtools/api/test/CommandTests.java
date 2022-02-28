f789b2a0357297632bc88b0add25051d96fa1cee
Joris Guffens <joris.guffens@outlook.com>
2022-02-28 16:43:09 +0100

    fixed unit tests

--- api/src/test/java/com/minestombrick/commandtools/api/test/CommandTests.java
+++ api/src/test/java/com/minestombrick/commandtools/api/test/CommandTests.java
@@ -3,14 +3,13 @@ package com.minestombrick.commandtools.api.test;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.command.CommandManager;
 import net.minestom.server.command.builder.Command;
-import net.minestom.server.instance.block.Block;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.minestombrick.commandtools.api.command.CommandGroupBuilder;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CommandTests {
 
@@ -29,8 +28,10 @@ public class CommandTests {
         AtomicBoolean called = new AtomicBoolean(false);
         cmd.setDefaultExecutor((sender, context) -> called.set(true));
 
-        Command group = CommandGroupBuilder.of("foo").group("bar")
-                .withCommand(cmd).build();
+        Command group = CommandGroupBuilder.of("foo")
+                .group("bar", g -> g
+                        .withCommand(cmd))
+                .build();
 
         cm.register(group);
 
